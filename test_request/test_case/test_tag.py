109394f530b6c7db849f7162c83e0095cb11df36
jiayu520 <jiayu520000@outlook.com>
2020-05-26 22:45:49 +0800

    requests的po封装

--- test_request/test_case/test_tag.py
+++ test_request/test_case/test_tag.py
@@ -14,12 +14,19 @@ class TestTga:
        print(json.dumps(Tag().get(),indent=2))
         #print(Tag().add('hello'))
 
-    def test_add(self):
-        self.tag.get()
-        tags = self.tag.get()
-        if 'wangwu' in jsonpath(tags, '$..name'):
-            self.tag.delete()
-        #print(Tag().add())
+    def test_all(self):
+       tag_id = jsonpath(self.tag.get(), '$..tag[?(@.name=="wangwu")].id')
+       if not tag_id:
+           self.tag.delete(tag_id[0])
+       self.tag.add('wangwu')
+       tag_id = jsonpath(self.tag.get(), '$..tag[?(@.name=="wangwu")].id')[0]
+       self.tag.update(tag_id,'haha')
+
+        # self.tag.get()
+        # tags = self.tag.get()
+        # if 'wangwu' in jsonpath(tags, '$..name'):
+        #     self.tag.delete('wangwu')
+        # #print(Tag().add())
 
     def test_delete(self):
         print(Tag().delete())
